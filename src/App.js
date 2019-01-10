34b2a3c93b50c9262c40e0494eecd4f683ee1ca0
Romain <rmtestard@gmail.com>
2019-01-10 19:52:36 +0100

    connecting the store to main app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import Posts from './components/Posts';
 import Postform from './components/Postform';
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Postform />
-        <hr />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Postform />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
