cc68ea9234cd3ef051158855ef8f5c0376dadd04
Romain <rmtestard@gmail.com>
2019-01-10 20:18:40 +0100

    setting postReducer

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,1 +1,12 @@
 import { FETCH_POSTS, NEW_POST } from './actions/types';
+
+export const fetchPosts = () => dispatch => {
+  fetch('http://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(posts =>
+      dispatch({
+        type: FETCH_POSTS,
+        payload: posts
+      })
+    );
+};
